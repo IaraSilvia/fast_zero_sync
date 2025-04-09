88e6442d060627d5b68da1d2714f6209c402760b
Iara <iara@democrata.adlan>
2025-04-09 16:23:27 -0300

    atualização do conftest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,30 +1,14 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retonar_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_read_root_deve_retonar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_exercicio_ola_mundo_em_html():
-    client = TestClient(app)
-
-    response = client.get('/exercicio-html')
-
-    assert response.status_code == HTTPStatus.OK
-    assert '<h1> Olá Mundo </h1>' in response.text
-
-
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/users/',
         json={
@@ -38,4 +22,18 @@ def test_create_user():
         'username': 'alice',
         'email': 'alice@example.com',
         'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
     }
